ded8b36d79bb98f4e23975c4cc324067b912f36c
Gabriel Baruque <baruque@edu.unirio.br>
2022-10-07 21:59:13 -0300

    implementação da calculadora desafio

--- desafio_calculadora.rb
+++ desafio_calculadora.rb
@@ -1,23 +1,25 @@
 loop do
     print 'Selecione uma opção de cálculo: '
-    print '1 - Soma'
-    print '2 - Subtração'
-    print '3 - Divisão'
-    print '4 - Multiplicação'
+    print '1 - Soma\r\n'
+    print '2 - Subtração\r\n'
+    print '3 - Divisão\r\n'
+    print '4 - Multiplicação\r\n'
+    print '5 - Cancelar'
 
     opcao = gets.chomp.to_i
 
-    if opcao < 1 || opcao > 4
-        print 'Opção inválida.'
+    if opcao < 1 || opcao > 5
+        print 'Opção inválida. Encerrando o aplicativo...'
+        break
+    elsif opcao == 5
+        print 'Cancelando...'
+        break
     else
-        print 'Digite o primeiro número: '
+        puts 'Digite o primeiro número: \n'
         n1 = gets.chomp.to_i
-        print 'Digite o segundo número: '
+        print 'Digite o segundo número: \r\n'
         n2 = gets.chomp.to_i
-        calcula(n1, n2)
-    end
-    
-    def calcula(n1, n2)
+
         case opcao
         when 1
             puts n1+n2
@@ -28,6 +30,5 @@ loop do
         when 4
             puts n1*n2
         end
-    end
-        
+    end        
 end
