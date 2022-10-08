06721d618010bf47c26b910648ef2df1d709526e
Gabriel Baruque <baruque@edu.unirio.br>
2022-10-07 22:14:08 -0300

    desafio completo

--- desafio_calculadora.rb
+++ desafio_calculadora.rb
@@ -1,34 +1,39 @@
 loop do
-    print 'Selecione uma opção de cálculo: '
-    print '1 - Soma\r\n'
-    print '2 - Subtração\r\n'
-    print '3 - Divisão\r\n'
-    print '4 - Multiplicação\r\n'
-    print '5 - Cancelar'
+    puts 'Selecione uma opção de cálculo: '
+    puts '1 - Soma'
+    puts '2 - Subtração'
+    puts '3 - Divisão'
+    puts '4 - Multiplicação'
+    puts '5 - Cancelar'
 
     opcao = gets.chomp.to_i
 
     if opcao < 1 || opcao > 5
-        print 'Opção inválida. Encerrando o aplicativo...'
-        break
+        puts 'Opção inválida. Tente novamente...'
     elsif opcao == 5
-        print 'Cancelando...'
+        puts 'Cancelando...'
         break
     else
-        puts 'Digite o primeiro número: \n'
+        puts 'Digite o primeiro número: '
         n1 = gets.chomp.to_i
-        print 'Digite o segundo número: \r\n'
+        puts 'Digite o segundo número: '
         n2 = gets.chomp.to_i
 
         case opcao
         when 1
-            puts n1+n2
+            puts "O resultado da soma é #{n1+n2}"
         when 2
-            puts n1-n2
+            puts "O resultado da subtração é #{n1-n2}"
         when 3
-            puts n1/n2
+            puts "O resultado da divisão é #{n1/n2}"
         when 4
-            puts n1*n2
+            puts "O resultado da multiplicação é #{n1*n2}"
         end
-    end        
+    end 
+    
+    puts 'Deseja fazer outro calculo? (s/n)'
+    opcao2 = gets.chomp.to_i
+    if opcao2 == 'n'
+        break
+    end
 end
